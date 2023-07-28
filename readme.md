ea5752a41f3c5118df7dd0567e75a072a728c009
salatine <95940523+salatine@users.noreply.github.com>
2023-07-28 19:43:24 +0000

    add requested features

--- readme.md
+++ readme.md
@@ -6,9 +6,6 @@ translateMeow is a script that converts cat code into morse code and vice versa,
 
 translates to "hello world"!
 
-## Features to add
-- direct translation (from meow code to actual characters)
-- inverse translation (from morse code to meow code, or from characters to meow code).
 
 We are accepting contributions to the project, as it's relatively complex and extremely time demanding.
 
--- translateMeow.c
+++ translateMeow.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
 
 char* replaceWord(const char* s, const char* oldW,
-                const char* newW)
+                const char* newW, bool reverse)
 {
     char* result;
     int i, cnt = 0;
     int newWlen = strlen(newW);
     int oldWlen = strlen(oldW);
+
+    if (reverse) {
+        const char* temp = oldW;
+        oldW = newW;
+        newW = temp;
+    }
  
     // Counting the number of times old word
     // occur in the string
@@ -40,16 +47,52 @@ char* replaceWord(const char* s, const char* oldW,
     return result;
 }
 
+char* translateMorse(char* morse, bool reverse) {
+    char letters[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 
+                              'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 
+                              's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+    char* morseLetters[] = {".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", 
+                              ".---", "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", 
+                              "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."};
+    
+    for (int i = 0; i < sizeof(letters); i++) {
+        morse = replaceWord(morse, morseLetters[i], &letters[i], reverse);
+    }
+
+    return morse;
+}
+
+char* translateMeow(char* meow, bool reverse) {
+    return replaceWord(replaceWord(meow, "meow", ".", reverse), "rawr", "-", reverse);
+}
+
 int main(int argc, char *argv[]){
     if (argc == 1) {
         printf("No code to translate provided\n");
         return 1;
     }
 
-	char* meow = argv[1];
-	char* translated = NULL;
+    bool reverse = false, character = false;
 
-	translated = replaceWord(replaceWord(meow, "meow", "."), "rawr", "-");
-	printf("%s\n", translated);
+    char* meow = argv[1];
+    for (int i = 2; i < argc; i++) {
+        if (strcmp(argv[i], "-r") == 0) {
+            reverse = true;
+        }
+
+        if (strcmp(argv[i], "-c") == 0) {
+            character = true;
+        }
+    }
+
+    char* translated;
+
+    if (character) {
+        translated = translateMorse(translateMeow(meow, false), reverse);
+    } else {
+        translated = translateMeow(meow, reverse);
+    }
+
+    printf("%s\n", translated);
 	return 0;
 }
